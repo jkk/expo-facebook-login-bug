aed7542cb31437c16f248f3c85e81233dd8cbeae
Justin Kramer <jkkramer@gmail.com>
2021-11-12 11:58:24 -0500

    Add expo-facebook and login test

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Pressable, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import * as Facebook from "expo-facebook";
 
 export default function App() {
+  let [loginResult, setLoginResult] = React.useState(null);
+
+  async function onFacebookLogin() {
+    try {
+      await Facebook.initializeAsync({ appId: "1310861962685645" });
+      let result = await Facebook.logInWithReadPermissionsAsync({
+        permissions: ["public_profile", "email"]
+      });
+      setLoginResult(result);
+    } catch (err) {
+      Alert.alert(`${err.name}: ${err.message}`);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{paddingBottom: 20}}>
+        <Text style={{fontSize: 18, fontWeight: "bold"}}>
+          Expo Facebook Login Bug
+        </Text>
+      </View>
+      <View style={{paddingBottom: 20}}>
+        <Pressable onPress={onFacebookLogin}>
+          <Text style={{fontSize: 16, color: "blue"}}>
+            Continue with Facebook
+          </Text>
+        </Pressable>
+      </View>
+      <View>
+        <Text style={{fontSize: 16}}>
+          {JSON.stringify(loginResult, undefined, 2)}
+        </Text>
+      </View>
     </View>
   );
 }
